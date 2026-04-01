44ff5b0ed4fbabb5b0f51accdbb7d0e1f4b36f86
unknown <trkulja.jovana@gimnazijaso.edu.rs>
2026-04-01 02:03:50 +0200

    add project files

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scoreTex = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Zemlja = new System.Windows.Forms.PictureBox();
+            this.preprekaDonji = new System.Windows.Forms.PictureBox();
+            this.ptica = new System.Windows.Forms.PictureBox();
+            this.preprekaGornji = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.highScore = new System.Windows.Forms.Label();
+            this.gameoverLabel = new System.Windows.Forms.Label();
+            this.highScoree = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemlja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preprekaDonji)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preprekaGornji)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.LightSalmon;
+            this.scoreLabel.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.scoreLabel.Location = new System.Drawing.Point(31, 40);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(146, 46);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "SCORE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 44);
+            this.label2.TabIndex = 1;
+            // 
+            // scoreTex
+            // 
+            this.scoreTex.AutoSize = true;
+            this.scoreTex.BackColor = System.Drawing.Color.LightSalmon;
+            this.scoreTex.Font = new System.Drawing.Font("Showcard Gothic", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTex.Location = new System.Drawing.Point(180, 40);
+            this.scoreTex.Name = "scoreTex";
+            this.scoreTex.Size = new System.Drawing.Size(138, 44);
+            this.scoreTex.TabIndex = 6;
+            this.scoreTex.Text = "label1";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameStart);
+            // 
+            // Zemlja
+            // 
+            this.Zemlja.Image = global::FlappyBird.Properties.Resources.groundd;
+            this.Zemlja.Location = new System.Drawing.Point(-6, 366);
+            this.Zemlja.Name = "Zemlja";
+            this.Zemlja.Size = new System.Drawing.Size(816, 115);
+            this.Zemlja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Zemlja.TabIndex = 5;
+            this.Zemlja.TabStop = false;
+            // 
+            // preprekaDonji
+            // 
+            this.preprekaDonji.Image = global::FlappyBird.Properties.Resources.donjiii_removebg_preview;
+            this.preprekaDonji.Location = new System.Drawing.Point(362, 248);
+            this.preprekaDonji.Name = "preprekaDonji";
+            this.preprekaDonji.Size = new System.Drawing.Size(118, 337);
+            this.preprekaDonji.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.preprekaDonji.TabIndex = 4;
+            this.preprekaDonji.TabStop = false;
+            // 
+            // ptica
+            // 
+            this.ptica.Image = global::FlappyBird.Properties.Resources.pticurina_removebg_preview;
+            this.ptica.Location = new System.Drawing.Point(12, 175);
+            this.ptica.Name = "ptica";
+            this.ptica.Size = new System.Drawing.Size(54, 34);
+            this.ptica.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptica.TabIndex = 3;
+            this.ptica.TabStop = false;
+            // 
+            // preprekaGornji
+            // 
+            this.preprekaGornji.Image = global::FlappyBird.Properties.Resources.gornji_removebg_preview;
+            this.preprekaGornji.Location = new System.Drawing.Point(362, -210);
+            this.preprekaGornji.Name = "preprekaGornji";
+            this.preprekaGornji.Size = new System.Drawing.Size(118, 330);
+            this.preprekaGornji.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.preprekaGornji.TabIndex = 2;
+            this.preprekaGornji.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PeachPuff;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "highest score:";
+            // 
+            // highScore
+            // 
+            this.highScore.AutoSize = true;
+            this.highScore.BackColor = System.Drawing.Color.Transparent;
+            this.highScore.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highScore.Location = new System.Drawing.Point(204, 21);
+            this.highScore.Name = "highScore";
+            this.highScore.Size = new System.Drawing.Size(0, 23);
+            this.highScore.TabIndex = 8;
+            // 
+            // gameoverLabel
+            // 
+            this.gameoverLabel.AutoSize = true;
+            this.gameoverLabel.BackColor = System.Drawing.Color.Transparent;
+            this.gameoverLabel.Font = new System.Drawing.Font("Showcard Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameoverLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.gameoverLabel.Location = new System.Drawing.Point(66, 151);
+            this.gameoverLabel.Name = "gameoverLabel";
+            this.gameoverLabel.Size = new System.Drawing.Size(414, 79);
+            this.gameoverLabel.TabIndex = 9;
+            this.gameoverLabel.Text = "GAME OVER!";
+            this.gameoverLabel.Visible = false;
+            // 
+            // highScoree
+            // 
+            this.highScoree.AutoSize = true;
+            this.highScoree.BackColor = System.Drawing.Color.PeachPuff;
+            this.highScoree.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highScoree.Location = new System.Drawing.Point(178, 17);
+            this.highScoree.Name = "highScoree";
+            this.highScoree.Size = new System.Drawing.Size(0, 20);
+            this.highScoree.TabIndex = 10;
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.resetButton.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.Firebrick;
+            this.resetButton.Location = new System.Drawing.Point(207, 233);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(104, 37);
+            this.resetButton.TabIndex = 11;
+            this.resetButton.Text = "Try again";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Visible = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.gameoverLabel);
+            this.Controls.Add(this.scoreTex);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.highScoree);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.highScore);
+            this.Controls.Add(this.Zemlja);
+            this.Controls.Add(this.preprekaDonji);
+            this.Controls.Add(this.ptica);
+            this.Controls.Add(this.preprekaGornji);
+            this.Controls.Add(this.label2);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.onKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.onKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Zemlja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preprekaDonji)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preprekaGornji)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox preprekaGornji;
+        private System.Windows.Forms.PictureBox ptica;
+        private System.Windows.Forms.PictureBox preprekaDonji;
+        private System.Windows.Forms.PictureBox Zemlja;
+        private System.Windows.Forms.Label scoreTex;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label highScore;
+        private System.Windows.Forms.Label gameoverLabel;
+        private System.Windows.Forms.Label highScoree;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gameTimer.Start();
+        }
+
+        int obstacleSpeed = 5;
+        int g = 5;
+        int score  = 0;
+        int gap, y;
+        bool isGameOver = false;
+        int gravity = 1;
+        int velocity = 0; // brzina ptice
+
+
+        int highscore = Properties.Settings.Default.Highscore;
+
+        private void gameOver()
+        {
+            if (isGameOver) return;   
+            isGameOver = true;
+
+            gameTimer.Stop();
+            //MessageBox.Show("Game Over!\nScore: " + score);
+            this.KeyPreview = true;
+            gameoverLabel.Visible = true;
+            resetButton.Visible = true;
+           // this.Focus();
+
+            if (score > highscore)
+            {
+                highscore = score;
+                Properties.Settings.Default.Highscore = highscore; // sačuvaj
+                Properties.Settings.Default.Save();
+            }
+            highScoree.Text = Convert.ToString(highscore);
+        }
+
+        Random r = new Random();
+        private void gameStart(object sender, EventArgs e)
+        {
+            if (isGameOver) return;
+            // highScoree.Text = Math.Max(score, highscore).ToString();
+
+            if (jumping)
+            {
+                velocity = -8;   // impuls skoka
+                jumping = false; 
+            }
+
+            velocity += gravity;  // gravitacija stalno povlači dole
+            ptica.Top += velocity;
+
+            if (ptica.Top < 0)
+                ptica.Top = 0;
+
+
+            preprekaDonji.Left -= obstacleSpeed;
+            preprekaGornji.Left -= obstacleSpeed;
+
+            if (preprekaDonji.Left < -150)
+            {
+                int novaPozicija = this.Width + 50;
+
+                preprekaDonji.Left = novaPozicija;
+                preprekaGornji.Left = novaPozicija;
+
+                gap = 130;
+                y = r.Next(50, 200);   // povećala sam opseg
+
+                preprekaDonji.Top = y + gap;
+                preprekaGornji.Top = y - preprekaGornji.Height;
+
+                score++;
+            }
+
+            if (ptica.Bounds.IntersectsWith(preprekaDonji.Bounds) || ptica.Bounds.IntersectsWith(preprekaGornji.Bounds) || ptica.Bounds.IntersectsWith(Zemlja.Bounds))
+            {
+                gameOver();
+            }
+
+
+            scoreTex.Text = Convert.ToString(score);
+
+           /* if (g > 0)
+                ptica.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            else
+                ptica.Image.RotateFlip(RotateFlipType.Rotate270FlipNone); */
+
+
+        }
+
+        bool jumping = false;
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                jumping = true;
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                jumping = false;
+        }
+
+
+        private void reset()
+        {
+            velocity = 0;
+
+
+            gameTimer.Stop();
+            isGameOver = false;
+            jumping = false;
+            //g = 0;
+
+            ptica.Top = ClientRectangle.Height/2;
+            preprekaDonji.Left = this.Width/2 + 200;
+            preprekaGornji.Left = this.Width / 2 + 200;
+
+            gameoverLabel.Visible = false;
+            resetButton.Visible = false;
+
+            gap = 130; // otvor između cevi
+            y = r.Next(50, 200);
+
+            preprekaDonji.Top = y + gap; 
+            preprekaGornji.Top = y - preprekaGornji.Height;
+
+            score = 0;
+            scoreTex.Text = Convert.ToString(score);
+            obstacleSpeed = 5;
+            g = 5;
+
+
+            highscore = Properties.Settings.Default.Highscore;
+            highScoree.Text = highscore.ToString();
+
+            gameTimer.Start();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
